bf9ceef3c87c7811600b4ad21f568febd4699d3b
Michael Alarcon <malarconii@yahoo.com>
2017-06-06 18:00:17 -0700

    Finished & fully functional web form with Caesar encryption

--- main.py
+++ main.py
@@ -9,19 +9,19 @@ form = """
 <html>
     <head>
         <style>
-            form {
+            form {{
                 background-color: #eee;
                 padding: 20px;
                 margin: 0 auto;
                 width 540px;
                 font 16px sans-serif;
                 border-radius: 10px;
-            }
-            textarea {
+            }}
+            textarea {{
                 margin: 10px 0;
                 width 540px;
                 height: 120px;
-            }
+            }}
         </style>
     </head>
     <body>
@@ -29,7 +29,7 @@ form = """
             <label for="rot">Rotate by:</label>
             <input type="text" name="rot" value="0" />
             <br />
-            <textarea name="text"></textarea>
+            <textarea name="text">{0}</textarea>
             <br />
             <input type="submit" />
         </form>
@@ -39,7 +39,7 @@ form = """
 
 @app.route("/")
 def index():
-    return form
+    return form.format("")
 
 @app.route("/", methods=['POST'])
 def encrypt():
@@ -48,7 +48,7 @@ def encrypt():
     newtext = ''
     for char in text:
         newtext += rotate_character(char, rot)
-    return '<h1>' + newtext + '</h1>'
+    return form.format(newtext)
 
 
 app.run()
